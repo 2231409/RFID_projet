2c9aec9e4b448b888d0cd4d4a5d498a598fe2f4b
Christophe15 <2034412@cegepba.qc.ca>
2024-10-11 11:36:03 -0400

    Ajout fonction buzzer

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import RPi.GPIO as GPIO
+import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
+
+def play_buzzer():
+    GPIO.output(13, GPIO.HIGH)
+    time.sleep(0.4)
+    GPIO.output(13, GPIO.LOW)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+play_buzzer()
